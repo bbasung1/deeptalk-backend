6acdcbe3c6fe4efe9e08c864cffa2beca1a2e42e
bbasung <bbasung1@daum.net>
2025-04-02 14:23:07 +0100

    [Feat]oauth.js
    get oauth code[WIP]

--- oauth.js
+++ oauth.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const dotenv = require("dotenv");
+
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+dotenv.config();
 
+router.get("/test/kakao", (req, res) => {
+    res.redirect(
+      process.env.KAKAO_AUTH_URL +
+      process.env.CLIENT_ID +
+      "&redirect_uri=" +
+      process.env.REDIRECT_URI +
+      "&scope=openid&response_type=code"
+    );
+  });
+
+  router.get("/kakao", (req, res) => {
+    let code = req.query.code;
+    res.json({
+      success: 1,
+      code: code
+    });
+  });
 module.exports = router;
