8c80a88083debd6e538348d775992cb65d746ce1
bbasung <bbasung1@daum.net>
2025-04-11 03:25:04 +0100

    [FEAT]oauth.js
    add signup feature(WIP)

--- oauth.js
+++ oauth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dotenv = require("dotenv");
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
+const knex = require("./knex.js");
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
@@ -50,7 +51,80 @@ router.get("/test/kakao", (req, res) => {
       console.log(data1.data);
       let kkoidtkn = jwt.decode(data1.data.id_token);
       let access_token = data1.data.access_token;
-      res.json(data1.data);
+      knex.select("*").from("user").where("kakao_id",kkoidtkn.sub).then((userdata)=>{
+        if(userdata){
+          data1.data.registerd=1;
+        }else{
+          data1.data.registerd=0;
+        }
+        res.json(data1.data);
+      });
     })
   })
+
+  router.put("/signup", (req, res) => {
+    let tkn = req.headers.authorization.split("Bearer ")[1];
+    let decodetoken = jwt.decode(tkn);
+    let iss = decodetoken.iss;
+    knex.transaction((trx) => {
+      let kakaoid=null;
+      let kakaoAccessCode = null;
+      let kakaoRefreshCode = null;
+      let kakaoIdToken = null;
+      let appleAccessCode = null;
+      let appleRefreshCode = null;
+      let appleIdToken = null;
+      if (iss == "https://kauth.kakao.com") {
+        kakaoAccessCode = req.body.access_token;
+        kakaoRefreshCode = req.body.refresh_token;
+        kakaoIdToken = tkn;
+        kakaoid=decodetoken.sub;
+      }
+      if (iss == "https://appleid.apple.com") {
+        appleAccessCode = req.body.access_token;
+        appleRefreshCode = req.body.refresh_token;
+        appleIdToken = tkn;
+      }
+      return trx
+        .insert(
+          {
+            kakao_access_code: kakaoAccessCode,
+            kakao_refresh_code: kakaoRefreshCode,
+            kakao_id_token: kakaoIdToken,
+            apple_access_code: appleAccessCode,
+            apple_refresh_code: appleRefreshCode,
+            apple_id_token: appleIdToken,
+            kakao_id: kakaoid,
+            email: "bbasung@test.com"
+          },
+          "id"
+        )
+        .into("user")
+        // .then((ids) => {
+        //   return trx
+        //     .insert({
+        //       id: ids[0],
+        //       nickname: req.body.name,
+        //       profile_image: req.body.profileImage,
+        //     })
+        //     .into("profile")
+        //     .then(() => {
+        //       let friends = req.body.friends;
+        //       if (friends.length > maxFriends) {
+        //         throw new Error("You have reached the maximum number of friends");
+        //       }
+        //       friends.forEach((friend) => {
+        //         friend.user_id = ids[0];
+        //       });
+        //       return trx.insert(friends).into("friend_list");
+        //     });
+        // })
+        .then(() => {
+          res.json({ success: 1 });
+        })
+        .catch((err) => {
+          res.json({ success: 0, error: err });
+        });
+    });
+  });
 module.exports = router;
