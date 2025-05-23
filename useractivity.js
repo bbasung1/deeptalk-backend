72f48b1870eecab8a0cdb33b89b7e1432169666d
bbasung <bbasung1@daum.net>
2025-05-23 05:38:42 +0100

    [FEAT]useractivity.js
    add useractivity api

---
+++ useractivity.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("./knex.js");
+const defind_id = require('./general.js').define_id;
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+
+router.post("/", async (req, res) => {
+    let user_id = req.body.id;
+    let id = await defind_id(user_id);
+    if (req.body.type == "talk") {
+        const talk = await knex('talk')
+            .where('writer_id', id)
+            .select('*');
+        res.json(talk);
+    }
+    if (req.body.type == "think") {
+        const think = await knex('think')
+            .where('writer_id', id)
+            .select('*');
+        res.json(think);
+    }
+    if (req.body.type == "comment") {
+        const user = await knex('comment')
+            .where('user_id', id)
+            .select("*");
+        res.json(user);
+    }
+})
+
+module.exports = router;
