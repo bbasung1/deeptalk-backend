3c1b49d9db0c601588801ce3e437b3bec3438081
bbasung <bbasung1@daum.net>
2025-05-27 03:04:18 +0100

    [FEAT]oauth.js
    edit oauth apple(WIP)

--- oauth.js
+++ oauth.js
@@ -10,27 +10,27 @@ router.use(express.urlencoded({ extended: true }));
 dotenv.config();
 
 router.get("/test/kakao", (req, res) => {
-    res.redirect(
-      process.env.KAKAO_AUTH_URL +
-      process.env.CLIENT_ID +
-      "&redirect_uri=" +
-      process.env.REDIRECT_URI +
-      "&scope=openid&response_type=code"
-    );
-  });
+  res.redirect(
+    process.env.KAKAO_AUTH_URL +
+    process.env.CLIENT_ID +
+    "&redirect_uri=" +
+    process.env.REDIRECT_URI +
+    "&scope=openid&response_type=code"
+  );
+});
 
-  router.get("/kakao", (req, res) => {
-    let code = req.query.code;
-    res.json({
-      success: 1,
-      code: code
-    });
+router.get("/kakao", (req, res) => {
+  let code = req.query.code;
+  res.json({
+    success: 1,
+    code: code
   });
+});
 
-  router.post("/kakao",(req,res)=>{
-    let code = req.body.code;
-    console.log(code);
-    axios
+router.post("/kakao", (req, res) => {
+  let code = req.body.code;
+  console.log(code);
+  axios
     .post(
       "https://kauth.kakao.com/oauth/token",
       {},
@@ -47,84 +47,146 @@ router.get("/test/kakao", (req, res) => {
         },
       }
     )
-    .then((data1)=>{
+    .then((data1) => {
       console.log(data1.data);
       let kkoidtkn = jwt.decode(data1.data.id_token);
       let access_token = data1.data.access_token;
-      knex.select("*").from("user").where("kakao_id",kkoidtkn.sub).then((userdata)=>{
-        if(userdata){
-          data1.data.registerd=1;
-        }else{
-          data1.data.registerd=0;
+      knex.select("*").from("user").where("kakao_id", kkoidtkn.sub).then((userdata) => {
+        if (userdata) {
+          data1.data.registerd = 1;
+        } else {
+          data1.data.registerd = 0;
         }
         res.json(data1.data);
       });
     })
-  })
+})
 
-  router.put("/signup", (req, res) => {
-    let tkn = req.body.jwt_token;
-    let decodetoken = jwt.decode(tkn);
-    let iss = decodetoken.iss;
-    knex.transaction((trx) => {
-      let kakaoid=null;
-      let kakaoAccessCode = null;
-      let kakaoRefreshCode = null;
-      let kakaoIdToken = null;
-      let appleAccessCode = null;
-      let appleRefreshCode = null;
-      let appleIdToken = null;
-      if (iss == "https://kauth.kakao.com") {
-        kakaoAccessCode = req.body.access_token;
-        kakaoRefreshCode = req.body.refresh_token;
-        kakaoIdToken = tkn;
-        kakaoid=decodetoken.sub;
-      }
-      if (iss == "https://appleid.apple.com") {
-        appleAccessCode = req.body.access_token;
-        appleRefreshCode = req.body.refresh_token;
-        appleIdToken = tkn;
-      }
-      return trx
-        .insert(
-          {
-            kakao_access_code: kakaoAccessCode,
-            kakao_refresh_code: kakaoRefreshCode,
-            kakao_id_token: kakaoIdToken,
-            apple_access_code: appleAccessCode,
-            apple_refresh_code: appleRefreshCode,
-            apple_id_token: appleIdToken,
-            kakao_id: kakaoid,
-            email: req.body.email
-          },
-          "id"
-        )
-        .into("user")
-        // .then((ids) => {
-        //   return trx
-        //     .insert({
-        //       id: ids[0],
-        //       nickname: req.body.name,
-        //       profile_image: req.body.profileImage,
-        //     })
-        //     .into("profile")
-        //     .then(() => {
-        //       let friends = req.body.friends;
-        //       if (friends.length > maxFriends) {
-        //         throw new Error("You have reached the maximum number of friends");
-        //       }
-        //       friends.forEach((friend) => {
-        //         friend.user_id = ids[0];
-        //       });
-        //       return trx.insert(friends).into("friend_list");
-        //     });
-        // })
-        .then(() => {
-          res.json({ success: 1 });
-        })
-        .catch((err) => {
-          res.json({ success: 0, error: err });
-        });
-    });
+router.get("/apple", (req, res) => {
+  connectioninfo(req);
+  const config = {
+    client_id: process.env.APPLE_CLIENT_ID,
+    redirect_uri: process.env.APPLE_LOGIN_REDIRECT_URL,
+    response_type: "code id_token",
+    state: "origin:web",
+    scope: "name email",
+    response_mode: "form_post",
+    m: 11,
+    v: "1.5.4",
+  };
+  const queryString =
+    `https://appleid.apple.com/auth/authorize?` +
+    Object.entries(config)
+      .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+      .join("&");
+  res.redirect(queryString);
+});
+const createSignWithAppleSecret = () => {
+  const token = jwt.sign({}, process.env.APPLE_SECRET_KEY, {
+    algorithm: "ES256",
+    expiresIn: "1h",
+    audience: "https://appleid.apple.com",
+    issuer: process.env.APPLE_TEAM_ID, // TEAM_ID
+    subject: process.env.APPLE_CLIENT_ID, // Service ID
+    keyid: process.env.APPLE_KEY_ID, // KEY_ID
   });
+  return token;
+};
+
+router.post("/callback/apple", (req, res) => {
+  connectioninfo(req);
+  let appleidtoken = req.body.id_token;
+  let idtwk = jwt.decode(appleidtoken);
+  let applesub = idtwk.sub;
+  let applecode = req.body.code;
+  let ci = req.body.ci;
+  axios
+    .post(
+      "https://appleid.apple.com/auth/token",
+      qs.stringify({
+        grant_type: "authorization_code",
+        code: applecode,
+        client_secret: createSignWithAppleSecret(),
+        client_id: process.env.APPLE_CLIENT_ID,
+        redirect_uri: process.env.APPLE_REDIRECT_URI,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    )
+    .then((data1) => {
+      let resinfo = data1.data;
+      console.log(resinfo);
+      res.json(resinfo);
+    });
+});
+
+
+router.put("/signup", (req, res) => {
+  let tkn = req.body.jwt_token;
+  let decodetoken = jwt.decode(tkn);
+  let iss = decodetoken.iss;
+  knex.transaction((trx) => {
+    let kakaoid = null;
+    let kakaoAccessCode = null;
+    let kakaoRefreshCode = null;
+    let kakaoIdToken = null;
+    let appleAccessCode = null;
+    let appleRefreshCode = null;
+    let appleIdToken = null;
+    if (iss == "https://kauth.kakao.com") {
+      kakaoAccessCode = req.body.access_token;
+      kakaoRefreshCode = req.body.refresh_token;
+      kakaoIdToken = tkn;
+      kakaoid = decodetoken.sub;
+    }
+    if (iss == "https://appleid.apple.com") {
+      appleAccessCode = req.body.access_token;
+      appleRefreshCode = req.body.refresh_token;
+      appleIdToken = tkn;
+    }
+    return trx
+      .insert(
+        {
+          kakao_access_code: kakaoAccessCode,
+          kakao_refresh_code: kakaoRefreshCode,
+          kakao_id_token: kakaoIdToken,
+          apple_access_code: appleAccessCode,
+          apple_refresh_code: appleRefreshCode,
+          apple_id_token: appleIdToken,
+          kakao_id: kakaoid,
+          email: req.body.email
+        },
+        "id"
+      )
+      .into("user")
+      // .then((ids) => {
+      //   return trx
+      //     .insert({
+      //       id: ids[0],
+      //       nickname: req.body.name,
+      //       profile_image: req.body.profileImage,
+      //     })
+      //     .into("profile")
+      //     .then(() => {
+      //       let friends = req.body.friends;
+      //       if (friends.length > maxFriends) {
+      //         throw new Error("You have reached the maximum number of friends");
+      //       }
+      //       friends.forEach((friend) => {
+      //         friend.user_id = ids[0];
+      //       });
+      //       return trx.insert(friends).into("friend_list");
+      //     });
+      // })
+      .then(() => {
+        res.json({ success: 1 });
+      })
+      .catch((err) => {
+        res.json({ success: 0, error: err });
+      });
+  });
+});
 module.exports = router;
