5cb8fb3be32998c25277e51f933fa1dc1b533241
bbasung <bbasung1@daum.net>
2025-12-29 03:59:25 +0000

    [FEAT]bookmark.js
    /bookmark/:id

--- bookmark.js
+++ bookmark.js
@@ -10,19 +10,19 @@ router.post("/:id", async (req, res) => {
     if (!ourid) {
         return res.status(400).json({ success: 0, msg: "id 인식 실패" });
     }
-    const [dupcheck] = await knex("post_like").select("*").where({ type: req.body.type, user_id: req.params.id })
+    const [dupcheck] = await knex("bookmark").select("*").where({ type: req.body.type, user_id: req.params.id })
     console.log(dupcheck);
     const trx = await knex.transaction();
     const type = req.body.type == 0 ? "talk" : "think";
     const num_name = type + "_num";
-    const [brf_bookmark] = await knex(type).select("mylist").where(num_name, req.params.id);
+    const [brf_bookmark] = await knex(type).select("bookmark").where(num_name, req.params.id);
     console.log(dupcheck);
     if (dupcheck != undefined) {
         try {
-            await trx("post_like").where({ type: req.body.type, post_id: req.params.id }).del();
+            await trx("bookmark").where({ type: req.body.type, post_id: req.params.id }).del();
             await trx(type).update({ like: brf_bookmark.like - 1 }).where(num_name, req.params.id);
             await trx.commit();
-            return res.json({ success: 1, msg: "좋아요 해제 완료" });
+            return res.json({ success: 1, msg: "북마크 해제 완료" });
         } catch (err) {
             console.error(err);
             return res.json({ success: 0 });
@@ -32,7 +32,7 @@ router.post("/:id", async (req, res) => {
         await trx("bookmark").insert({ user_id: ourid, type: req.body.type, post_id: req.params.id });
         await trx(type).update({ like: brf_bookmark.like + 1 }).where(num_name, req.params.id);
         await trx.commit();
-        return res.json({ success: 1, msg: "좋아요 완료" });
+        return res.json({ success: 1, msg: "북마크 완료" });
     } catch (err) {
         console.error(err);
         return res.json({ success: 0 });
