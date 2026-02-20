a22e0e3df2b66fc5c6a71804582cf1525d53f8b1
bbasung <bbasung1@daum.net>
2026-02-20 14:11:15 +0000

    [FIX]image.js
    fix error

--- profile/image.js
+++ profile/image.js
@@ -11,15 +11,17 @@ const fs = require("fs").promises;
 
 router.post("/", upload.single("file"), async (req, res) => {
     try {
-        const ourid=await define_id(req.headers.authorization, res);
+        const ourid = await define_id(req.headers.authorization, res);
         if (!ourid) return res.status(401).json({ success: false, message: "인증 실패" }); // 인증 실패 시 종료
         if (!req.file) {
             return res.status(400).json({ success: false, message: "파일이 필요합니다." });
         }
         const oldImage = await knex("profile").select("image").where({ id: ourid }).first();
-        if(oldImage!=null){
+        console.log(oldImage);
+        if (oldImage.image != null) {
             const oldImagePath = `${FILE_DIR}/${oldImage.image}`;
             try {
+                console.log(oldImagePath)
                 await fs.unlink(oldImagePath);
             } catch (err) {
                 return res.status(500).json({ success: false, message: "이전 이미지 삭제 실패" });
@@ -28,7 +30,7 @@ router.post("/", upload.single("file"), async (req, res) => {
         const ext = req.file.originalname.split(".").pop();
         const filename = `img_${ourid}.${ext}`;
 
-        const savedPath = await saveImage(req.file.buffer, filename,FILE_DIR);
+        const savedPath = await saveImage(req.file.buffer, filename, FILE_DIR);
         await knex("profile").update({ image: filename }).where({ id: ourid });
         return res.json({
             success: true,
