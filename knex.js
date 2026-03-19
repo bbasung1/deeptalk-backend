1f10786eb6c1e0818063f45ccb93596706a7b33d
bbasung <bbasung1@daum.net>
2026-03-19 05:25:19 +0000

    [FIX]oauth.js
    add pool connection

--- knex.js
+++ knex.js
@@ -1,12 +1,22 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const knex = require("knex")({
-  client: "mysql",
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
   },
+  pool: {
+    min: 0,
+    max: 500, // 적절히 조절 (서버 용량에 맞게)
+    acquireTimeoutMillis: 60000,
+    createTimeoutMillis: 30000,
+    idleTimeoutMillis: 30000,
+    reapIntervalMillis: 1000,
+  },
+  acquireConnectionTimeout: 60000,
+
 });
 module.exports = knex;
